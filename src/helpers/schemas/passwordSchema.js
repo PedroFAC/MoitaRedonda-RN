c26d96ded4cabbe14f471ad2204d6fb3bcb7e727
Pedro Facanha <pedro.facanha@hp.com>
2020-12-27 17:22:41 -0300

    Password schema

---
+++ src/helpers/schemas/passwordSchema.js
@@ -0,0 +1,13 @@
+import * as yup from 'yup';
+
+export const passwordSchema = yup.object({
+  password: yup
+    .string()
+    .required('Por favor insira uma senha')
+    .min(8, 'Senha precisa ter no mínimo 8 caracteres')
+    .max(16, 'Senha só pode ter no máximo 16 caracteres'),
+  confirmPassword: yup
+    .string()
+    .required('Por favor insira uma confirmação de senha')
+    .oneOf([yup.ref('password'), null], 'As senhas precisam ser iguais'),
+});
